81447475fc63724fe1634c25a1bfe9bad2a0cc22
yeliz601 <yeliz601@BPC11>
2023-12-12 12:01:20 +0300

    Proje dosyası ekle.

---
+++ YikiciMetotlar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YikiciMetotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program başladı.");
+            Otomobil oto = new Otomobil();
+            Console.WriteLine("Program bitti.");
+        }
+    }
+    class Otomobil
+    {
+        string marka = "";
+        string renk = "";
+        public Otomobil()
+        {
+            marka = "TOGG";
+            renk = "kırmızı";
+            Console.WriteLine("YAPİCİ METOT CALİSTİ.");
+        }
+        ~Otomobil()
+        {
+            Console.WriteLine("NESNE HAFİZADAN CİKTİ.");
+        }
+    }
+}
